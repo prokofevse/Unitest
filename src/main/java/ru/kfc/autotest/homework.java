43ef1b9643ce4f9c263eb66c6d4f0d7e40045a29
s.prokofiev <s.prokofiev@samolet.ru>
2025-06-05 17:01:56 +0400

    homework 1 lesson

--- src/main/java/ru/kfc/autotest/homework.java
+++ src/main/java/ru/kfc/autotest/homework.java
@@ -1,9 +1,40 @@
 package ru.kfc.autotest;
 
+import java.util.Random;
+
 public class homework {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world!");
-    }
+        int[] array = new int[20];
+        Random random = new Random();// Создаётся массив из 20 элементов
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50); // Заполнение массива случайными числами от 0 до 50
+        }
+
+        System.out.println();
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // Вывод элементов массива
+
+
+        int max = array[0];
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        } // Поиск наибольшего элемента
+
+
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        } // Подсчёт суммы элементов
+
+
+        System.out.println(max);
+        System.out.println(sum);
+    } // Вывод максимального значения и общей суммы всех значений из массива
 }
