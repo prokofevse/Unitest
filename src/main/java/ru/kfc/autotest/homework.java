11205370e6afde741442fe94ae0f97f25591df23
s.prokofiev <s.prokofiev@samolet.ru>
2025-06-05 13:51:54 +0400

    homework commit

---
+++ src/main/java/ru/kfc/autotest/homework.java
@@ -0,0 +1,9 @@
+package ru.kfc.autotest;
+
+public class homework {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world!!!!!");
+    }
+}
